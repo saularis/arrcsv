1b6dc4c7e967740925191c73ceca9c7efddc2449
Abdulla Malik <abdulla.coder@gmail.com>
2022-04-03 21:24:05 +0600

    add package dir

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 import setuptools
 
-setuptools.setup(name='arrcsv',
+setuptools.setup(
+    name='arrcsv',
     version='0.0.1',
     description='A python package for converting array to csv',
-    keywords = ['convert', 'array', 'csv'],
+    keywords = ['python', 'convert', 'array', 'csv'],
+    author='Abdulla Malik',
+    author_email='hi@abdulla.dev',
     url='https://github.com/saularis/arrcsv',
     download_url='https://github.com/saularis/arrcsv/archive/refs/tags/v0.0.1.tar.gz',
-    author='Abdulla Malik',
     install_requires=['numpy'],
-    author_email='hi@abdulla.dev',
     packages=setuptools.find_packages(),
-    zip_safe=False
+    package_dir={'arrcsv': 'arrcsv'},
 )
